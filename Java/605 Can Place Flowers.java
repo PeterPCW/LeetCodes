a070590d3b8fdb0fb8d2d9eca6e002efcafd1bb0
Peter Williams <49111837+PeterPCW@users.noreply.github.com>
2023-03-20 16:09:55 -0500

    Day12

--- Java/605 Can Place Flowers.java
+++ Java/605 Can Place Flowers.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int count = 0; // Variable to track the count of flowers you can place
         for (int i = 0; i < flowerbed.length; i++) { // Loop through ever spot in the flower bed...
             if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) { // If the current spot is open AND (it's the first plot OR the previous is empty) AND (it's the last plot OR the next one is empty)...
-                count++; // Increment the count because we found a viable plot...
-                flowerbed[i] = 1; // And "plant a flower" to prepare for the next plot's check
+                flowerbed[i] = 1; // "Plant a flower" to prepare for the next iteration...
+                n--; // And decreemnt the number of flowers left to plant
             }
         }
-        return count >= n; // Compare the plantable plots to the desired number of flowers to return true/false
+        return n <= 0; // If the number of flowers left to plant has reached/passed 0 then there were enough plots available
     }
 }
 
@@ -18,5 +17,5 @@ Explanation:
 Iterate over the flowerbed array to analyze each plot.
 The solution is quite simple but requires some logic to deem each plot as plantable. The if statement handles the 3 conditions for allowing a new flower: a) not already planted, b) no flower directly before, and c) no flower directly after.
 Modifying current plot in flowerbed[] sets the new state for the next plot check.
-Compare the final count with the requested n flower to provide a true/false answer.
+Using decrement on n instead of maintaining a separate count to compare saves memory at runtime (and a line of code). If enough plots are available, n will be decremented to 0 or lower.
 */
